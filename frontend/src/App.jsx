208a54a096a0cefd5002e68454387a269f35f36a
VanessaGrd <suede210810@gmail.com>
2023-07-09 17:26:10 +0200

    activityPage+buttonAdd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Choice from "./pages/Choice";
@@ -9,8 +8,7 @@ import ActivityList from "./components/ActivityList";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/register" element={<Register />} />
+      <Route path="/" element={<Register />} />
       <Route path="/login" element={<Login />} />
       <Route path="/choice" element={<Choice />} />
       <Route path="/activities" element={<ActivityList />} />
