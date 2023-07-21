2bf89211b399687b66532a380ba86769728a73ef
VanessaGrd <suede210810@gmail.com>
2023-07-21 09:16:40 +0200

    chatbot

---
+++ frontend/src/components/ChatBot.jsx
@@ -0,0 +1,110 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
+import { useUserContext } from "../contexts/UserContext";
+import "react-toastify/dist/ReactToastify.css";
+import logoutButton from "../assets/logout.svg";
+
+import styles from "./ChatBot.module.scss";
+import logo from "../assets/logo.png";
+
+export default function ChatBot() {
+  const [message, setMessage] = useState("");
+  const [chat, setChat] = useState([]);
+  const navigate = useNavigate();
+  const { logout } = useUserContext();
+
+  const handleSendMessage = () => {
+    setChat((prevChat) => [...prevChat, { content: message, sender: "user" }]);
+    setMessage("");
+
+    setTimeout(() => {
+      let botResponse = "Je ne comprends pas. Pouvez-vous répéter ?";
+      const userMessage = message.toLowerCase();
+
+      const pattern = /^(.+?)$/; // Regular expression to match the desired pattern
+      const match = userMessage.match(pattern);
+
+      if (match) {
+        const word = match[1].trim(); // Extract the word from the user message
+        // eslint-disable-next-line no-use-before-define
+        botResponse = getTranslation(word); // Get the translation for the extracted word
+      }
+
+      setChat((prevChat) => [
+        ...prevChat,
+        { content: botResponse, sender: "bot" },
+      ]);
+    }, 1000);
+  };
+  // Fonction de déconnexion
+  const handleLogout = () => {
+    logout();
+    toast.success("Déconnexion réussie !");
+    navigate("/login");
+  };
+  const getTranslation = (word) => {
+    // Ajoutez ici un dictionnaire avec les traductions en suédois pour les mots de la vie courante
+    const translations = {
+      bonjour: "hej",
+      merci: "tack",
+      oui: "ja",
+      non: "nej",
+      pardon: "ursäkta",
+      maison: "hus",
+      chat: "katt",
+      chien: "hund",
+      eau: "vatten",
+      nourriture: "mat",
+      voiture: "bil",
+    };
+
+    const translation = translations[word];
+    if (translation) {
+      return `En suédois, "${word}" se dit "${translation}".`;
+    }
+    return `Désolé, je ne connais pas la traduction de "${word}" en suédois.`;
+  };
+
+  return (
+    <div className={styles.chatbot}>
+      <div className={styles.header}>
+        <div className={styles.logo}>
+          <img src={logo} alt="logo" />
+        </div>
+        <button
+          className={styles.logoutButton}
+          type="button"
+          onClick={handleLogout}
+        >
+          {" "}
+          <img src={logoutButton} alt="logout-button" />
+        </button>
+      </div>
+      <h1>ChatBot</h1>
+      <div className={styles.chatbox}>
+        {chat.map((messages, index) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <p key={index} className={messages.sender}>
+            {messages.content}
+          </p>
+        ))}
+      </div>
+      <div>
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button
+          className={styles.buttonHandle}
+          type="button"
+          onClick={handleSendMessage}
+        >
+          Envoyer
+        </button>
+      </div>
+    </div>
+  );
+}
